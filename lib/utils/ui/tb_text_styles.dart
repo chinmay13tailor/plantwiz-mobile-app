254d1ca05fc061f7247bd0772d34f160210d8758
chinmay13tailor <157699339+chinmay13tailor@users.noreply.github.com>
2026-05-08 16:36:31 +0530

    tb_text_styles.dart

--- lib/utils/ui/tb_text_styles.dart
+++ lib/utils/ui/tb_text_styles.dart
@@ -1,77 +1,76 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 abstract final class TbTextStyles {
-  static final titleXs = GoogleFonts.roboto(
+  static final titleXs = TextStyle(
     fontWeight: FontWeight.w500,
     fontSize: 18,
     height: 1.33,
     letterSpacing: .15,
   );
 
-  static final titleSmallSb = GoogleFonts.roboto(
+  static final titleSmallSb = TextStyle(
     fontWeight: FontWeight.w600,
     fontSize: 20,
     letterSpacing: .01,
     height: 1.2,
   );
 
-  static final titleMedium = GoogleFonts.roboto(
+  static final titleMedium = TextStyle(
     fontWeight: FontWeight.w500,
     fontSize: 24,
     height: 1.33,
   );
 
-  static final titleLarge = GoogleFonts.roboto(
+  static final titleLarge = TextStyle(
     fontWeight: FontWeight.w500,
     fontSize: 28,
     height: 1.28,
     letterSpacing: .25,
   );
 
-  static final labelSmall = GoogleFonts.roboto(
+  static final labelSmall = TextStyle(
     fontWeight: FontWeight.w400,
     fontSize: 12,
     height: 1.14,
     letterSpacing: .4,
   );
 
-  static final labelMedium = GoogleFonts.roboto(
+  static final labelMedium = TextStyle(
     fontWeight: FontWeight.w500,
     fontSize: 14,
     letterSpacing: .25,
     height: 1.4,
   );
 
-  static final labelLarge = GoogleFonts.roboto(
+  static final labelLarge = TextStyle(
     fontWeight: FontWeight.w500,
     fontSize: 16,
     letterSpacing: .25,
     height: 1.5,
   );
 
-  static final bodySmall = GoogleFonts.roboto(
+  static final bodySmall = TextStyle(
     fontWeight: FontWeight.w400,
     fontSize: 11,
     height: 1.45,
     letterSpacing: .25,
   );
 
-  static final bodyMedium = GoogleFonts.roboto(
+  static final bodyMedium = TextStyle(
     fontWeight: FontWeight.w400,
     fontSize: 14,
     letterSpacing: .2,
     height: 1.4,
   );
 
-  static final bodyRegular = GoogleFonts.roboto(
+  static final bodyRegular = TextStyle(
     fontWeight: FontWeight.w400,
     fontSize: 17,
     height: 1.5,
     letterSpacing: -0.41,
   );
 
-  static final bodyLarge = GoogleFonts.roboto(
+  static final bodyLarge = TextStyle(
     fontWeight: FontWeight.w400,
     fontSize: 16,
     letterSpacing: .15,
