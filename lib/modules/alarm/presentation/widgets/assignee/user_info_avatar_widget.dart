a80690057103be200fa9c07105162eb6228d65aa
chinmay13tailor <157699339+chinmay13tailor@users.noreply.github.com>
2026-05-08 16:34:28 +0530

    user_info_avatar_widget.dart

--- lib/modules/alarm/presentation/widgets/assignee/user_info_avatar_widget.dart
+++ lib/modules/alarm/presentation/widgets/assignee/user_info_avatar_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class UserInfoAvatarWidget extends StatelessWidget {
   const UserInfoAvatarWidget({
@@ -24,7 +23,7 @@ class UserInfoAvatarWidget extends StatelessWidget {
       child: Center(
         child: Text(
           shortName,
-          style: GoogleFonts.roboto(
+          style: const TextStyle(
             fontWeight: FontWeight.w700,
             fontSize: 14,
             letterSpacing: 1,
